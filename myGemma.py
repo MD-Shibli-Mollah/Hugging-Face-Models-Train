bfbdb140c701fafda0ae1c5340959fc2e7630203
MD SHIBLI MOLLAH <md.ta.shibli@gmail.com>
2024-03-06 22:29:54 +0600

    Q/A updated

--- myGemma.py
+++ myGemma.py
@@ -1,10 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Access token is required!
 access_token = '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'
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b", token = access_token)
 model = AutoModelForCausalLM.from_pretrained("google/gemma-2b", token = access_token)
 
-input_text = "Write me a poem about Machine Learning."
+input_text = "I want to know about MBZUAI."
 input_ids = tokenizer(input_text, return_tensors="pt")
 
 outputs = model.generate(**input_ids)
